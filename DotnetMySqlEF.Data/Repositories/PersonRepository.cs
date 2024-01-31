11f3496af596428f0436333fa2c28d6043139d18
2000jz <royacesparks@gmail.com>
2024-02-01 01:58:18 +0530

    Add project files.

---
+++ DotnetMySqlEF.Data/Repositories/PersonRepository.cs
@@ -0,0 +1,48 @@
+﻿using DotnetMySqlEF.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotnetMySqlEF.Data.Repositories
+{
+    public class PersonRepository : IPersonRepository
+    {
+        private readonly PersonContext _ctx;
+        public PersonRepository(PersonContext ctx)
+        {
+            _ctx = ctx;
+        }
+
+        public async Task<IEnumerable<Person>> GetPeopleAsync()
+        {
+            var people = await _ctx.People.ToListAsync();
+            return people;
+        }
+        public async Task<Person> GetPeopleByIdAsync(int id)
+        {
+            return await _ctx.People.FindAsync(id);
+
+        }
+        public async Task<Person> CreatePersonAsync(Person person)
+        {
+            _ctx.People.Add(person);
+            await _ctx.SaveChangesAsync();
+            return person;
+        }
+
+        public async Task UpdatePersonAsync(Person person)
+        {
+            _ctx.People.Update(person);
+            await _ctx.SaveChangesAsync();
+        }
+
+        public async Task DeletePersonAsync(Person person)
+        {
+            _ctx.People.Remove(person);
+            await _ctx.SaveChangesAsync();
+        }
+    }
+}
